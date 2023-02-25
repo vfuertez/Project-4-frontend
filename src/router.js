eb8476222ac39788ec266dc5375c9ec513b6628b
Vincent Fuertez <vfuertez@gmail.com>
2023-02-25 00:54:51 -0500

    added component and pages folder

--- src/router.js
+++ src/router.js
@@ -1,12 +1,18 @@
-import {createBrowserRouter, createRoutesFromElements, Route, Routes} from "react-router-dom"
+import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom"
 import App from "./App"
+import Index from "./pages/Index"
+import Show from "./pages/Show"
 
 const router = createBrowserRouter(createRoutesFromElements(
-    <Routes>
+    <>
         <Route path="/" element={<App/>}>
-
+            <Route path="" element={<Index/>}/>
+            <Route path="post/:id" element={<Show/>} />
+            <Route path="create" />
+            <Route path="update/:id"/>
+            <Route path="delete/:id"/>
         </Route>
-    </Routes>
+    </>
 ))
 
 export default router
