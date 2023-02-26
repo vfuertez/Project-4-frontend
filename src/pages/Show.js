4dfa298275f4e2be0431caf3d8d1cafd11f4fe06
Vincent Fuertez <vfuertez@gmail.com>
2023-02-25 20:42:12 -0500

    full crud complete

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -7,24 +7,51 @@ const Show = (props) => {
     <>
       <div>
         <h2>{post.name}</h2>
-       <a href={post.repo1} target='_blank' rel="noopener noreferrer"> {post.repo1} </a>  
+        <a href={post.repo1} target="_blank" rel="noopener noreferrer">
+          {' '}
+          {post.repo1}{' '}
+        </a>
         <h3>{post.repo2}</h3>
         <h3>{post.web}</h3>
-
-        <Link to="/">
-          <button> Main Page</button>
-        </Link>
       </div>
 
       <div>
         <Form action={`/update/${post.id}`} method="post">
-          <input type="text" name="name" placeholder="name" defaultValue={post.name} />
-          <input type="text" name="repo1" placeholder="Repo1" defaultValue={post.repo1} />
-          <input type="text" name="repo2" placeholder="Repo2" defaultValue={post.repo2} />
-          <input type="text" name="web" placeholder="website" defaultValue={post.web} />
+          <input
+            type="text"
+            name="name"
+            placeholder="name"
+            defaultValue={post.name}
+          />
+          <input
+            type="text"
+            name="repo1"
+            placeholder="Repo1"
+            defaultValue={post.repo1}
+          />
+          <input
+            type="text"
+            name="repo2"
+            placeholder="Repo2"
+            defaultValue={post.repo2}
+          />
+          <input
+            type="text"
+            name="web"
+            placeholder="website"
+            defaultValue={post.web}
+          />
           <button>Update</button>
         </Form>
+
+        <Form action={`/delete/${post.id}`} method="post">
+            <button>Delete</button>
+        </Form>
       </div>
+
+      <Link to="/">
+        <button> Main Page</button>
+      </Link>
     </>
   )
 }
